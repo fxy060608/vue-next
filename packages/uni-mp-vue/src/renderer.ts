b9998dd197e2eea487243552277bbefe63cb77f6
fxy060608 <fxy060608@gmail.com>
2021-12-13 17:13:52 +0800

    fix(mp): assigning effect to instance

--- packages/uni-mp-vue/src/renderer.ts
+++ packages/uni-mp-vue/src/renderer.ts
@@ -280,11 +280,11 @@ function setupRenderEffect(instance: ComponentInternalInstance) {
   }
 
   // create reactive effect for rendering
-  const effect = new ReactiveEffect(
+  const effect = (instance.effect = new ReactiveEffect(
     componentUpdateFn,
     () => queueJob(instance.update),
     instance.scope // track it in component's effect scope
-  )
+  ))
 
   const update = (instance.update = effect.run.bind(effect) as SchedulerJob)
   update.id = instance.uid
