602d9e01d7dc0381fcc0beee6826a278d1c4a69d
zhenyuWang <13641039885@163.com>
2022-12-07 16:05:45 +0800

    feat(devtools): export devtools functions

--- packages/uni-mp-vue/src/index.ts
+++ packages/uni-mp-vue/src/index.ts
@@ -3,3 +3,8 @@ export { diff } from './diff'
 export { patch } from './patch'
 export function withModifiers() {}
 export function createVNode() {}
+export {
+  devtoolsComponentUpdated,
+  devtoolsComponentRemoved,
+  devtoolsComponentAdded
+} from '@vue/runtime-core'
