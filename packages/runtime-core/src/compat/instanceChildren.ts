57268bd2f2eb015c35a35638a57c539ff7e3c32a
wangyaqi <wangyaqi988@163.com>
2024-02-21 15:25:49 +0800

    Revert "feat(uni-app-x web): 补充组件实例的$children属性，忽略内置组件"
    This reverts commit 9f3578305a0d2ca8aeef109104a739d92033545e.

--- packages/runtime-core/src/compat/instanceChildren.ts
+++ packages/runtime-core/src/compat/instanceChildren.ts
@@ -7,9 +7,7 @@ import { assertCompatEnabled, DeprecationTypes } from './compatConfig'
 export function getCompatChildren(
   instance: ComponentInternalInstance
 ): ComponentPublicInstance[] {
-  if (!__X__) {
-    assertCompatEnabled(DeprecationTypes.INSTANCE_CHILDREN, instance)
-  }
+  assertCompatEnabled(DeprecationTypes.INSTANCE_CHILDREN, instance)
   const root = instance.subTree
   const children: ComponentPublicInstance[] = []
   if (root) {
@@ -18,17 +16,9 @@ export function getCompatChildren(
   return children
 }
 
-function isInternalComponent(p: ComponentPublicInstance | null) {
-  return p && p.$options && (p.$options.__reserved || p.$options.rootElement)
-}
-
 function walk(vnode: VNode, children: ComponentPublicInstance[]) {
   if (vnode.component) {
-    if (!__X__ || (__X__ && !isInternalComponent(vnode.component.proxy))) {
-      children.push(vnode.component.proxy!)
-    } else if (__X__ && isInternalComponent(vnode.component.proxy)) {
-      walk(vnode.component.subTree, children)
-    }
+    children.push(vnode.component.proxy!)
   } else if (vnode.shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     const vnodes = vnode.children as VNode[]
     for (let i = 0; i < vnodes.length; i++) {
