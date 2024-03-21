3f33bd37ebaf677c6ec9e6d8d82be13169fd3d7a
jixinbao <jixinbao@dcloud.io>
2024-03-21 15:17:07 +0800

    fix(x): 处理 button 样式数据视为对象进行处理

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -518,9 +518,9 @@ function createBaseVNode(
   // 保持和微信一致
   // button 如果没有设置hoverClass属性，需要指定默认值
   if (type == 'button') {
-    if (vnode.props == null) vnode.props = new Map()
-    if (!vnode.props!.has('hoverClass') && !vnode.props!.has('hover-class')) {
-      vnode.props!.set('hoverClass', 'button-hover')
+    if (vnode.props == null) vnode.props = {}
+    if (!vnode.props!['hoverClass'] && !vnode.props!['hover-class']) {
+      vnode.props!['hoverClass'] = 'button-hover'
     }
   }
 
