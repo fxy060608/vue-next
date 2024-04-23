d0ba5950cab9df66a69aba141ed80340c9a64860
jixinbao <jixinbao@dcloud.io>
2024-04-23 21:57:00 +0800

    feat: 补充 css 展开解析

--- packages/runtime-x/src/modules/style.ts
+++ packages/runtime-x/src/modules/style.ts
@@ -13,6 +13,13 @@ import {
 } from '../helpers/node'
 import { parseStyleDecl } from './style/parser'
 
+function isSame(a: any | null, b: any | null): boolean {
+  return (isString(a) && isString(b)) ||
+    (typeof a === 'number' && typeof b === 'number')
+    ? a == b
+    : a === b
+}
+
 export function patchStyle(
   el: UniXElement,
   prev: NormalizedStyle | string,
@@ -33,20 +40,21 @@ export function patchStyle(
   const isPrevObj = prev && !isString(prev)
 
   if (isPrevObj) {
+    // 获取 class 和 style 上的样式
     const classStyle = getExtraClassStyle(el)
-
     const style = getExtraStyle(el)
+    // 修改 prev
     for (const key in prev) {
       const _key = camelize(key)
-      if (next[_key] == null) {
-        // const value = next[key]
+      if (next[key] == null) {
+        // 先读取 class 样式
         const value =
           classStyle != null && classStyle.has(_key)
             ? classStyle!.get(_key)
             : ''
 
         // 传递简写 css kye value => Map [[key, value]]
-        parseStyleDecl(key, value).forEach((value: any, key: string) => {
+        parseStyleDecl(_key, value).forEach((value: any, key: string) => {
           batchedStyles.set(key, value)
           // 把style中的样式移除掉，否则style的优先级始终比class高
           style?.delete(key)
@@ -57,9 +65,8 @@ export function patchStyle(
     }
     for (const key in next) {
       const value = next[key]
-      // const prevValue = prev[key]
-      // if (!isSame(prevValue, value))
-      {
+      const prevValue = prev[key]
+      if (!isSame(prevValue, value)) {
         parseStyleDecl(camelize(key), value).forEach(
           (value: any, key: string) => {
             batchedStyles.set(key, value)
