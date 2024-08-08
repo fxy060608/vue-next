c3148d2d5b3b62080ce26b6d5a1b3d84046cc2d5
fxy060608 <153647646@qq.com>
2024-08-08 11:00:28 +0800

    wip(types): remove h with HTMLElementTagNameMap

--- packages/dts-test/h.test-d.ts
+++ packages/dts-test/h.test-d.ts
@@ -11,7 +11,7 @@ import {
   ref,
   resolveComponent,
 } from 'vue'
-import { describe, expectAssignable, expectType } from './utils'
+import { describe, expectAssignable /*, expectType */ } from './utils'
 
 describe('h inference w/ element', () => {
   // key
@@ -35,16 +35,17 @@ describe('h inference w/ element', () => {
   const slots = { default: () => {} } // RawSlots
   h('div', {}, slots)
   // events
-  h('div', {
-    onClick: e => {
-      expectType<MouseEvent>(e)
-    },
-  })
-  h('input', {
-    onFocus(e) {
-      expectType<FocusEvent>(e)
-    },
-  })
+  // fixed by xxxxxx
+  // h('div', {
+  //   onClick: e => {
+  //     expectType<MouseEvent>(e)
+  //   },
+  // })
+  // h('input', {
+  //   onFocus(e) {
+  //     expectType<FocusEvent>(e)
+  //   },
+  // })
 })
 
 describe('h inference w/ Fragment', () => {
