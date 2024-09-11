4976f51f647f005e1e4a8d60ffeb199a0d70c0f9
DCloud_UNI_BFC <baofuchun@dcloud.io>
2024-09-11 15:28:39 +0800

    fix: 动态删除children子节点时机调整

--- packages/uni-mp-vue/src/renderer.ts
+++ packages/uni-mp-vue/src/renderer.ts
@@ -405,27 +405,27 @@ function setupRenderEffect(instance: ComponentInternalInstance) {
 
 function unmountComponent(instance: ComponentInternalInstance) {
   const { bum, scope, update, um } = instance
+  // beforeUnmount hook
+  if (bum) {
+    invokeArrayFns(bum)
+  }
   if (__FEATURE_OPTIONS_API__) {
     // $children
     const parentInstance = instance.parent
     if (parentInstance) {
-      let $children = parentInstance.ctx.$children as (
+      const $children = parentInstance.ctx.$children as (
         | ComponentPublicInstance
         | Record<string, any>
       )[]
 
-      let target = getExposeProxy(instance) || instance.proxy
-      let index = $children.indexOf(target!)
+      const target = getExposeProxy(instance) || instance.proxy
+      const index = $children.indexOf(target!)
       if (index > -1) {
         $children.splice(index, 1)
       }
     }
   }
 
-  // beforeUnmount hook
-  if (bum) {
-    invokeArrayFns(bum)
-  }
   // stop effects in component scope
   scope.stop()
   // update may be null if a component is unmounted before its async
