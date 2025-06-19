7883a8051a2f63177108092c7add3f0e124d753f
jixinbao <jixinbao@dcloud.io>
2025-06-19 16:45:09 +0800

    feat(x): 优化 parseStyleDecl 修复单元测试错误

--- packages/runtime-x/src/modules/style/parser/index.ts
+++ packages/runtime-x/src/modules/style/parser/index.ts
@@ -1,61 +1,36 @@
 // @ts-expect-error esm js
 import { expand } from '@dcloudio/uni-nvue-styler/dist/uni-nvue-styler.es'
-type Declaration = any
-
-const processDeclaration = expand({ type: 'uvue' }).Declaration as (
-  decl: Declaration,
-) => void
-
-function createDeclaration(prop: string, value: unknown) {
-  const newValue = value + ''
-  if (newValue.includes('!important')) {
-    return {
-      prop,
-      value: newValue.replace(/\s*!important/, ''),
-      important: true,
-    }
-  }
-  return {
-    prop,
-    value: newValue,
-    important: false,
-  }
+export type Declaration = {
+  prop: string
+  value: string
+  important: boolean
+  replaceWith: (newProps: Declaration[]) => void
 }
 
-function normalizeStyle(name: string, value: unknown) {
-  const decl = Object.assign(
-    {},
-    {
-      replaceWith(newProps: Declaration[]) {
-        props = newProps
-      },
+/**
+ * Declaration 的扩展，包含 expand 样式展开
+ */
+const processDeclaration: (decl: Declaration) => void = expand({
+  type: 'uvue',
+}).Declaration
+
+/**
+ * 解析 style，返回 Declaration[]
+ */
+export function parseStyleDecl(prop: string, value: any | null): Declaration[] {
+  const newValue = String(value)
+  const isImportant = newValue.includes('!important')
+
+  const decl: Declaration = {
+    prop,
+    value: isImportant ? newValue.replace(/\s*!important/, '') : newValue,
+    important: isImportant,
+    replaceWith(newProps: Declaration[]) {
+      props = newProps
     },
-    createDeclaration(name, value),
-  ) as Declaration
+  }
 
   let props = [decl]
   processDeclaration(decl)
   return props
-}
-
-function setStyle(expandRes: any[]) {
-  const resArr = expandRes.map(item => {
-    return [item.prop as string, item.value as string]
-  }) as Array<[string, string]>
-  // to Map
-  const resMap = new Map(resArr)
-  return resMap
-}
-
-/**
- * 解析 style，返回 Map
- * eg: width, null => map [['width', '']]
- */
-export function parseStyleDecl(
-  prop: string,
-  value: any | null,
-): Map<string, any> {
-  const val = normalizeStyle(prop, value)
-  const res = setStyle(val)
-  return res
 }
