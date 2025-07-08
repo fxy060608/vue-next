062aac218b529e547f83bd4f4f2591ec1f7dbdb5
fxy060608 <fxy060608@gmail.com>
2025-07-08 11:27:32 +0800

    feat:  mountPage/unmountPage

--- packages/runtime-x/src/index.ts
+++ packages/runtime-x/src/index.ts
@@ -1,8 +1,12 @@
 import {
+  type AppContext,
+  type ComponentPublicInstance,
   type CreateAppFunction,
   type Renderer,
   type RootRenderFunction,
   createRenderer,
+  createVNode,
+  type defineComponent,
 } from '@vue/runtime-core'
 import { nodeOps, setDocument } from './nodeOps'
 import { patchProp } from './patchProp'
@@ -48,6 +52,35 @@ export const createApp = ((...args) => {
 
   return app
 }) as CreateAppFunction<UniXElement>
+
+export function createMountPage(appContext: AppContext) {
+  return function mountPage(
+    pageComponent: ReturnType<typeof defineComponent>,
+    pageProps: Record<string, any>,
+    pageContainer: UniXElement,
+  ): ComponentPublicInstance {
+    const vnode = createVNode(pageComponent, pageProps)
+    // store app context on the root VNode.
+    // this will be set on the root instance on initial mount.
+    vnode.appContext = appContext
+    ;(vnode as any).__page_container__ = pageContainer
+    render(vnode, pageContainer)
+    const publicThis = vnode.component!.proxy!
+    ;(publicThis as any).__page_container__ = pageContainer
+    return publicThis
+  }
+}
+
+export function unmountPage(pageInstance: ComponentPublicInstance): void {
+  const { __page_container__ } = pageInstance as any
+  if (__page_container__) {
+    __page_container__.isUnmounted = true
+    render(null, __page_container__)
+    delete (pageInstance as any).__page_container__
+    const vnode = pageInstance.$.vnode
+    delete (vnode as any).__page_container__
+  }
+}
 
 // SFC CSS utilities
 export { useCssModule } from './helpers/useCssModule'
